8d8c7972179bfd48dd1a28fd541ed94bb99acb6f
Daniel Eberle <daniel.j.eberle@protonmail.com>
2024-05-23 23:19:07 -0500

    exact path for secrets

--- system/users/mm/secrets.nix
+++ system/users/mm/secrets.nix
@@ -1,12 +1,12 @@
 {config, ...}: {
   age.secrets = {
     tofu = {
-      file = "${config.users.users.mm.home}/.secrets/tofu.age";
+      file = "/home/mm/.secrets/tofu.age";
       mode = "600";
       owner = "mm";
     };
     minio = {
-      file = "${config.users.users.mm.home}/.secrets/minio.age";
+      file = "/home/mm/.secrets/minio.age";
       mode = "600";
       owner = "mm";
     };
