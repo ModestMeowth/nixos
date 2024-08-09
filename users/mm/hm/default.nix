8f4e52922dbf6a18a5dad7dcc6f81a5e78d434ad
ModestMeowth <email@example.com>
2024-08-08 22:22:44 -0500

    split home-manager by host

--- users/mm/hm/default.nix
+++ users/mm/hm/default.nix
@@ -60,30 +60,4 @@
       };
     };
   };
-
-  hm-mm = {
-    browser.firefox = true;
-    editor.nvim.profile = "development";
-    gaming.retroarch = true;
-    githubKeys = true;
-    git = {
-      enable = true;
-      commitizen = true;
-      gh = true;
-      lazygit = true;
-    };
-    shell = {
-      fish.profile = "multiplexer";
-      gnome = {
-        enable = true;
-
-        dracula = true;
-        forge = true;
-        gsconnect = true;
-        no-overview = true;
-        user-themes = true;
-      };
-    };
-    term.wezterm = true;
-  };
 }
