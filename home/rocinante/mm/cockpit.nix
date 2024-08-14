d9cf3a471c361475121db035c6b4940aeb768a8f
Daniel Eberle <daniel.j.eberle@protonmail.com>
2024-08-13 19:58:11 -0500

    Remove old config

--- home/rocinante/mm/cockpit.nix
+++
@@ -1,7 +0,0 @@
-{
-  cockpit.settings.WebService = {
-    Origins = "https://localhost:9090 wss://localhost:9090 https://rocinante.lan wss://rocinante.lan";
-    ProtocolHeader = "X-Forwarded-Proto";
-    ForwardedForHeader = "X-Forwarded-For";
-  };
-}
