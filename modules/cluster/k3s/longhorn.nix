30d87e41143e4006b5bf9987763e3ab6af3afc07
Daniel Eberle <daniel.j.eberle@protonmail.com>
2024-10-02 21:46:59 -0500

    longhorn

--- modules/cluster/k3s/longhorn.nix
+++ modules/cluster/k3s/longhorn.nix
@@ -2,11 +2,17 @@
   options.hostConfig.cluster.k3s-longhorn = lib.mkEnableOption "k3s-longhorn";
 
   config = lib.mkIf config.hostConfig.cluster.k3s {
+    boot.supportedFilesystems = [ "nfs" ];
+
     environment.systemPackages = [ pkgs.nfs-utils ];
 
-    services.openiscsi = {
-      enable = true;
-      name = "{config.networking.hostName}-initiatorHost";
+    services = {
+      openiscsi = {
+        enable = true;
+        name = "{config.networking.hostName}-initiatorHost";
+      };
+
+      rpcbind.enable = true;
     };
 
     systemd.tmpfiles.rules = [
