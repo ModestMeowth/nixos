88b5f6cceea4b7e9d25712dce50846913b5ab5b8
Daniel Eberle <daniel.j.eberle@protonmail.com>
2024-10-02 21:37:01 -0500

    longhorn

--- modules/cluster/k3s/longhorn.nix
+++ modules/cluster/k3s/longhorn.nix
@@ -4,15 +4,13 @@
   config = lib.mkIf config.hostConfig.cluster.k3s {
     environment.systemPackages = [ pkgs.nfs-utils ];
 
-    services = {
-      tmpfiles.rules = [
-        "L+ /usr/local/bin - - - - /run/current-system/sw/bin/"
-      ];
-
-      openiscsi = {
-        enable = true;
-        name = "{config.networking.hostName}-initiatorHost";
-      };
+    services.openiscsi = {
+      enable = true;
+      name = "{config.networking.hostName}-initiatorHost";
     };
+
+    systemd.tmpfiles.rules = [
+      "L+ /usr/local/bin - - - - /run/current-system/sw/bin/"
+    ];
   };
 }
