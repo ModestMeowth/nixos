a9aca9f6313f3ba7fe5e0b042041bb1109828daa
ModestMeowth <email@example.com>
2024-10-11 01:30:24 -0500

    move ssh keys from flake to packages

--- users/mm/default.nix
+++ users/mm/default.nix
@@ -1,5 +1,6 @@
 {config, lib, pkgs, ...}: let
   ifGroupsExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+  sourceData = pkgs.callPackage ../../packages/_sources/generated.nix {};
 in {
   users.users.mm = {
     isNormalUser = true;
@@ -17,5 +18,9 @@ in {
     ];
 
     initialHashedPassword = "$y$j9T$xgIkUu0jxDn.E27xw3HIP0$AxOebMJ322FjxN2ncCvz8g0HWhdn3Om.d9HyWyV35K0";
+
+    openssh.authorizedKeys.keyFiles = [
+      sourceData.keys.src
+    ];
   };
 }
