a9b1a660a146c16004937abe11ebf26dd4efb5a2
ModestMeowth <email@example.com>
2024-10-12 14:04:37 -0500

    zellij

--- home/mm/shell.nix
+++ home/mm/shell.nix
@@ -17,6 +17,11 @@
     set fzf_preview_dir_cmd eza --always --color=always
     set -x FZF_DEFAULT_OPTS --color="fg:#f8f8f2,bg:#282a36,hl:#bd93f9,fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9,info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6,marker:#ff79c6,spinner:#ffb86c,header:#6272a4" --cycle --layout-reverse --border --height=90% --preview-window=wrap --marker="*"
     fish_vi_key_bindings
+
+    if not string match -qi "vscode" $TERM_PROGRAM
+      set ZELLIJ_AUTO_ATTACH true
+      set ZELLIJ_AUTO_EXIT true
+    end
   '';
 
   programs.fish.shellAbbrs = {
@@ -59,10 +64,13 @@
   xdg.configFile."starship.toml".source = "${inputs.mm.outputs.dotfiles}/dot_config/starship.toml.tmpl";
 
 
-  programs.zellij.enable = true;
+  programs.zellij = {
+    enable = true;
+    enableFishIntegration = true;
+  };
   xdg.configFile = {
     "zellij/config.kdl".source = "${inputs.mm.outputs.dotfiles}/dot_config/zellij/config.kdl";
-    "zellij/layouts.default.kdl".source = "${inputs.mm.outputs.dotfiles}/dot_config/zellij/layouts/default.kdl";
+    "zellij/layouts/default.kdl".source = "${inputs.mm.outputs.dotfiles}/dot_config/zellij/layouts/default.kdl";
     "zellij/themes/dracula.kdl".source = "${inputs.dracula.outputs.zellij}";
   };
 }
