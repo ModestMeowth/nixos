0f4b40b9a49e184ff9e1ccf4419addac1fe5f516
Daniel Eberle <daniel.j.eberle@protonmail.com>
2024-12-19 17:32:58 -0600

    fix zfs-reblance usage

--- packages/zfs-rebalance.nix
+++ packages/zfs-rebalance.nix
@@ -3,8 +3,9 @@
   scriptName = "zfs-rebalance";
   deps = [pkgs.perl];
   scriptSrc = sourceData.zfs-inplace-rebalancing."zfs-inplace-rebalancing.sh";
-  script = (pkgs.writeScriptBin "zfs-rebalance" scriptSrc).overrideAttrs(old: {
-    buildCommand = "${old.buildCommand}\n patchShebangs $out";
+  script = (pkgs.writeScriptBin "zfs-rebalance" (
+    builtins.replaceStrings ["zfs-inplace-rebalancing"] ["zfs-rebalance"] scriptSrc)).overrideAttrs(old: {
+      buildCommand = "${old.buildCommand}\n patchShebangs $out";
   });
 in pkgs.symlinkJoin {
   name = scriptName;
