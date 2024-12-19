f387d45b2c6d32bcba219b8eaa4e3a0c08003a59
Daniel Eberle <daniel.j.eberle@protonmail.com>
2024-12-19 16:22:53 -0600

    add zfs-inplace-rebalancing to packages

--- packages/default.nix
+++ packages/default.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 {
-  dracula = pkgs.callPackage ./dracula.nix { };
-  fish-plugins = pkgs.fishPlugins.callPackage ./fish-plugins.nix { };
-  keys = pkgs.callPackage ./keys.nix { };
-  neovim-plugins = pkgs.callPackage ./neovim-plugins.nix { };
+  dracula = pkgs.callPackage ./dracula.nix {};
+  fish-plugins = pkgs.fishPlugins.callPackage ./fish-plugins.nix {};
+  keys = pkgs.callPackage ./keys.nix {};
+  neovim-plugins = pkgs.callPackage ./neovim-plugins.nix {};
+  zfs-rebalance = pkgs.callPackage ./zfs-inplace-rebalancing.nix {};
 }
