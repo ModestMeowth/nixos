50d35ef3e47a436b709e19857b91e27acef0696f
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-01-16 18:29:54 -0600

    fix yamlls tags for home-assistant

--- home/_modules/neovim/lsp.nix
+++ home/_modules/neovim/lsp.nix
@@ -142,6 +142,12 @@ in
           lspconfig.yamlls.setup {
             settings = {
               yaml = {
+                customTags = {
+                  "!include_dir_list",
+                  "!include_dir_merge_list",
+                  "!include_dir_merge_named",
+                  "!include_dir_named",
+                },
                 schemas = schemastore.yaml.schemas(),
                 schemaStore = {
                   enable = false,
