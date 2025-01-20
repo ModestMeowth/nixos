11bc40519deec7c937a94068d02d0272be074337
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-01-19 23:21:22 -0600

    fix hostdev?

--- hosts/pwnyboy/vms/home-assistant.nix
+++ hosts/pwnyboy/vms/home-assistant.nix
@@ -1,5 +1,10 @@
-stuff @ {config, lib, inputs, pkgs, ...}:
-let
+stuff @ {
+  config,
+  lib,
+  inputs,
+  pkgs,
+  ...
+}: let
   cfg = config.virtualisation.libvirt;
   virt = inputs.virt;
   template = ../../../templates/nixvirt/home-assistant.nix;
@@ -15,16 +20,23 @@ in {
         storage_vol.volume = "home-assistant.qcow2";
         nvram_path = "/persist/vm/nvram/home-assistant.fd";
         mac_address = "52:54:00:72:24:39";
-        hostdev = {
-          mode = "subsystem";
-          type = "usb";
-          managed = true;
-          source.vendor.id = 4292; # 0x10c4
-          source.product.id = 60000; # ea60
-          address.type = "usb";
-          address.bus = 0;
-          address.port = 1;
-        };
+        hostdev = [
+          # SkyConnect
+          # Silicon Labs CP210x UART Bridge
+          {
+            alias.name = "hostdev0";
+            mode = "subsystem";
+            type = "usb";
+            managed = true;
+
+            source.vendor.id = 4292; # 0x10c4
+            source.product.id = 60000; # ea60
+
+            address.type = "usb";
+            address.bus = 0;
+            address.port = 1;
+          }
+        ];
       });
     }
   ];
