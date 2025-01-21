054009191d7a267928d8cca1f01a7925ada9fd36
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-01-21 02:22:37 -0600

    use systemd-resolved, add mdns + dnssec + dnsovertls

--- hosts/rocinante/networks.nix
+++ hosts/rocinante/networks.nix
@@ -6,10 +6,21 @@ in
     services.NetworkManager-wait-online.enable = false;
   };
 
+  services = {
+    resolved.enable = true;
+    resolved.dnssec = "true";
+    resolved.dnsovertls = "opportunistic";
+    resolved.extraConfig = ''
+      [Resolve]
+      MulticastDNS=yes
+    '';
+  };
+
   networking = {
     hostName = "rocinante";
 
     networkmanager.enable = true;
+    networkmanager.dns = "systemd-resolved";
     networkmanager.ensureProfiles.profiles = {
       "Ponyboy Bounce House".connection.interface-name = wifiInterface;
       "Hyrule".connection.interface-name = wifiInterface;
