9efb4d75441d40d47d1032302637ce07f717d493
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-08-10 20:39:55 -0500

    more tidying

--- hosts/pwnyboy/default.nix
+++ hosts/pwnyboy/default.nix
@@ -1,12 +1,127 @@
-{
+{ config, lib, pkgs, ... }:
+let
+  bridge = "bridge0";
+  cidr = "192.168.0";
+  prefix = 24;
+in {
   imports = [
-    ./filesystems.nix
     ./hardware.nix
-    # ./monitoring
-    ./networks.nix
-    ./samba
     ./secrets.nix
-    ./services.nix
+
+    ./samba
     ./vms
   ];
+
+  networking = {
+    hostName = "pwnyboy";
+    bridges."${bridge}".interfaces = [ "enp3s0" ];
+
+    useDHCP = false;
+
+    interfaces."${bridge}".ipv4.addresses = [{
+      address = "${cidr}.30";
+      prefixLength = prefix;
+    }];
+
+    defaultGateway = {
+      address = "${cidr}.1";
+      interface = "${bridge}";
+    };
+
+    nameservers = [ "${cidr}.1" "1.1.1.1" ];
+
+    firewall = {
+      trustedInterfaces = [ "tailscale0" "docker0" ];
+
+      allowedTCPPorts = [
+        80
+        443 # HTTP
+        53
+        853 # DNS over TCP
+        8123 # HomeAssistant
+        22000 # Syncthing
+      ];
+
+      allowedUDPPorts = [
+        53 # DNS
+        22000
+        21027 # Syncthing
+      ];
+    };
+  };
+
+  services = {
+    chrony.enable = true;
+
+    tailscale = {
+      enable = true;
+      package = pkgs.unstable.tailscale;
+      authKeyFile = config.sops.secrets."tskey".path;
+      extraSetFlags = [ "--ssh" "--webclient" "--accept-routes" ];
+    };
+
+    fwupd.enable = true;
+
+    nix-serve.enable = true;
+    nix-serve.secretKeyFile = config.sops.secrets."cache-priv-key".path;
+    nix-serve.openFirewall = true;
+
+    samba.enable = true;
+    samba.openFirewall = true;
+
+    samba.settings.global = {
+      "workgroup" = "WORKGROUP";
+      "server string" = "pwnyboy";
+      "netbios name" = "pwnyboy";
+
+      "security" = "user";
+      "guest account" = "nobody";
+      "map to guest" = "bad user";
+
+      "mangled names" = "no";
+      "vfs objects" = "catia";
+      "catia:mappings" = "0x3a:0x5f";
+    };
+
+    samba-wsdd.enable = true;
+    samba-wsdd.openFirewall = true;
+
+    samba.settings.media = {
+      "path" = "/persist/data/media";
+      "public" = "yes";
+      "browsable" = "yes";
+      "read only" = "no";
+      "writeable" = "yes";
+      "guest ok" = "no";
+      "create mask" = "0644";
+      "directory mask" = "0755";
+      "force user" = "mm";
+      "force group" = "users";
+    };
+  };
+
+  power.ups = {
+    enable = true;
+
+    upsmon.settings = {
+      MINSUPPLIES = 0;
+      POWERDOWNFLAG = null;
+    };
+
+    ups."apc" = {
+      driver = "usbhid-ups";
+      port = "auto";
+      directives = [ "vendorid = 051d" "productid = 0002" ];
+    };
+  };
+
+  environment.systemPackages = with pkgs; [ docker-compose ];
+
+  virtualisation = {
+    docker.enable = true;
+    libvirt.enable = true;
+  };
+
+  systemd.services."libvirt-guests".enable = lib.mkForce false;
+  virtualisation.libvirt.swtpm.enable = true;
 }
--- hosts/pwnyboy/hardware.nix
+++ hosts/pwnyboy/hardware.nix
@@ -1,4 +1,8 @@
-{ lib, modulesPath, pkgs, ... }: {
+{ lib, modulesPath, pkgs, ... }:
+let
+  root = "zroot/pwnyboy";
+  persist = "zroot/persist";
+in {
   imports = [ ("${modulesPath}/installer/scan/not-detected.nix") ];
 
   boot = {
@@ -44,4 +48,42 @@
       after = [ "dev-ttyUSB0.device" ];
     };
   };
+
+  fileSystems = {
+    "/" = {
+      device = "${root}/root";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+
+    "/nix" = {
+      device = "${root}/nix";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+    };
+
+    "/persist/etc" = {
+      device = "${persist}/etc";
+      fsType = "zfs";
+      options = [ "zfsutil" ];
+      neededForBoot = true;
+    };
+
+    "/boot" = {
+      device =
+        "/dev/disk/by-id/nvme-INTEL_SSDPEDMW400G4_CVCQ5252008B400AGN-part1";
+      fsType = "vfat";
+      options = [ "dmask=0077" "fmask=0177" ];
+    };
+  };
+
+  boot.zfs.extraPools = [ "docker" "persist" ];
+
+  services.sanoid.datasets = {
+    "${persist}".useTemplate = [ "default" ];
+    "persist/backups".useTemplate = [ "default" ];
+    "persist/cloud".useTemplate = [ "default" ];
+  };
+
+  shares = { ha-config.enable = true; };
 }
