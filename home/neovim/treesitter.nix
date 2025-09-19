bd16cece3b9e887a3066c216941551d1e6f8a2bb
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-09-19 16:08:54 -0500

    clean up home-manager, add rainbow-delimiters to neovim

--- home/neovim/treesitter.nix
+++ home/neovim/treesitter.nix
@@ -1,17 +1,21 @@
 {pkgs, ...}: {
-  programs.nixvim.plugins.treesitter = {
-    enable = true;
+  programs.nixvim.plugins = {
+    treesitter = {
+      enable = true;
 
-    grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars;
+      grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars;
 
-    settings = {
-      auto_install = false;
-      fold.enable = true;
-      highlight = {
-        enable = true;
-        additional_vim_regex_highlighting = true;
+      settings = {
+        auto_install = false;
+        fold.enable = true;
+        highlight = {
+          enable = true;
+          additional_vim_regex_highlighting = true;
+        };
+        indent.enable = true;
       };
-      indent.enable = true;
     };
+
+    rainbow-delimiters.enable = true;
   };
 }
