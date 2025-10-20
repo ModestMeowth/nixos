0cf45768a9fc44f4b24ea8d2f24a1ed4f3bfb497
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-10-20 01:51:38 -0500

    trusted-users

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -29,6 +29,10 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
+
+      trusted-users = [
+        "mm"
+      ];
     };
   };
 }
