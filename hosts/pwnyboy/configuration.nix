84c1a7f1c3a7e5eb387840beacf81c81aaf1dd60
Daniel Eberle <daniel.j.eberle@protonmail.com>
2026-01-23 01:41:06 -0600

    forgot about nut

--- hosts/pwnyboy/configuration.nix
+++ hosts/pwnyboy/configuration.nix
@@ -72,6 +72,32 @@
     };
   };
 
+  power.ups = {
+    enable = true;
+    mode = "netserver";
+    openFirewall = true;
+
+    upsd.listen = [
+      { address = "0.0.0.0"; }
+      { address = "::"; }
+    ];
+
+    users."ha".passwordFile = config.sops.secrets."ha-nut-user".path;
+
+    ups."network" = {
+      driver = "usbhid-ups";
+      description = "Networking UPS";
+      port = "auto";
+      directives = ["vendorid = 051d" "productid = 0002"];
+    };
+
+    upsmon.monitor."network" = {
+      system = "network@192.168.1.30";
+      user = "ha";
+      passwordFile = config.sops.secrets."ha-nut-user".path;
+    };
+  };
+
   services = {
     chrony.enable = true;
     tailscale =
