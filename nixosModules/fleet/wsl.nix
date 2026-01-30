697e540561ae178a67ce66613f310644056e0b84
Daniel Eberle <daniel.j.eberle@protonmail.com>
2026-01-30 16:31:12 -0600

    wsl tweaks

--- nixosModules/fleet/wsl.nix
+++ nixosModules/fleet/wsl.nix
@@ -6,6 +6,10 @@ in
   options.fleet.isWsl = lib.mkEnableOption "device is WSL";
 
   config = lib.mkIf cfg {
-    services.chrony.enable = lib.mkForce false;
+    services = {
+      chrony.enable = lib.mkForce false;
+      udisks2.enable = lib.mkForce false;
+      upower.enable = lib.mkForce false;
+    };
   };
 }
