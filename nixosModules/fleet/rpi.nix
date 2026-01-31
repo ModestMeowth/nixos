627446fc828a968e9a4e66952d4d35f82b1bf166
Daniel Eberle <daniel.j.eberle@protonmail.com>
2026-01-31 00:17:06 -0600

    rpi stuff

--- nixosModules/fleet/rpi.nix
+++ nixosModules/fleet/rpi.nix
@@ -8,5 +8,9 @@ in
   config = lib.mkIf cfg {
     nix.settings.max-jobs = 0;
     networking.networkmanager.enable = true;
+
+    services = {
+      chrony.enable = lib.mkForce false;
+    };
   };
 }
