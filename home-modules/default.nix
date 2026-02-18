add5c430d942b0443564b86a4b81c8a82465e91f
Modest Meowth <modest@meowth.dev>
2026-02-18 14:24:43 -0600

    use extra-substituters

--- home-modules/default.nix
+++ home-modules/default.nix
@@ -54,6 +54,9 @@
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+      ];
+
+      extra-substituters = [
         "https://catppuccin.cachix.org"
         "https://hyprland.cachix.org"
         "https://walker.cachix.org"
@@ -63,6 +66,9 @@
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+
+      extra-trusted-public-keys = [
         "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
--- nixos-modules/default.nix
+++ nixos-modules/default.nix
@@ -52,6 +52,9 @@
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+      ];
+
+      extra-substituters = [
         "https://lanzaboote.cachix.org"
         "https://hyprland.cachix.org"
         "https://catppuccin.cachix.org"
@@ -62,6 +65,9 @@
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+
+      extra-trusted-public-keys = [
         "lanzaboote.cachix.org-1:Nt9//zGmqkg1k5iu+B3bkj3OmHKjSw9pvf3faffLLNk="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
