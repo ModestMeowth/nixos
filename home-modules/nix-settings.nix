ab8d6566b84619e99f0028b7c6b325c0b2c8a84e
Modest Meowth <modest@meowth.dev>
2026-02-22 19:45:14 -0600

    nix-settings in separate module

---
+++ home-modules/nix-settings.nix
@@ -0,0 +1,52 @@
+{pkgs, ...}:
+{
+  nix = {
+    package = pkgs.nix;
+    distributedBuilds = true;
+    buildMachines = [
+      {
+        hostName = "pwnyboy";
+        sshUser = "mm";
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+      }
+    ];
+
+    settings = {
+      experimental-features = "nix-command flakes";
+
+      auto-optimise-store = true;
+      keep-outputs = true;
+      keep-derivations = true;
+
+      allowed-users = [ "@wheel" ];
+      trusted-users = [ "@wheel" ];
+
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+
+      extra-substituters = [
+        "https://catppuccin.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://walker.cachix.org"
+        "https://walker-git.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+
+      extra-trusted-public-keys = [
+        "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+        "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
+      ];
+    };
+  };
+}
--- nixos-modules/default.nix
+++ nixos-modules/default.nix
@@ -8,82 +8,19 @@
 {
   system.stateVersion = "25.11";
 
-  imports =
-    with inputs;
-    [
-      sops-nix.nixosModules.sops
-      nix-index-database.nixosModules.nix-index
-      stylix.nixosModules.stylix
-      catppuccin.nixosModules.catppuccin
-    ]
-    ++ (with ezModules; [
-      # networkmanager
-      secrets
-      theme
-      users
-    ]);
+  imports = [
+    inputs.sops-nix.nixosModules.sops
+    inputs.nix-index-database.nixosModules.nix-index
+    inputs.stylix.nixosModules.stylix
+    inputs.catppuccin.nixosModules.catppuccin
+    ezModules.nix-settings
+    ezModules.secrets
+    ezModules.theme
+    ezModules.users
+  ];
 
   i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = "America/Chicago";
-
-  nix = {
-    distributedBuilds = true;
-    buildMachines = [
-      {
-        hostName = "pwnyboy";
-        sshUser = "mm";
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-        ];
-      }
-      {
-        hostName = "thoughtpolice";
-        sshUser = "mm";
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-        ];
-      }
-    ];
-
-    settings = {
-      experimental-features = "nix-command flakes";
-
-      auto-optimise-store = true;
-      keep-outputs = true;
-      keep-derivations = true;
-
-      allowed-users = [ "@wheel" ];
-      trusted-users = [ "@wheel" ];
-
-      substituters = [
-        "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-      ];
-
-      extra-substituters = [
-        "https://lanzaboote.cachix.org"
-        "https://hyprland.cachix.org"
-        "https://catppuccin.cachix.org"
-        "https://walker.cachix.org"
-        "https://walker-git.cachix.org"
-      ];
-
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-
-      extra-trusted-public-keys = [
-        "lanzaboote.cachix.org-1:Nt9//zGmqkg1k5iu+B3bkj3OmHKjSw9pvf3faffLLNk="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
-        "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
-        "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
-      ];
-    };
-  };
 
   documentation = {
     doc.enable = lib.mkDefault false;
@@ -196,5 +133,4 @@
     udisks2.enable = true;
     upower.enable = true;
   };
-
 }
