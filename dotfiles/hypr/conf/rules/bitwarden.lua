482e52bd15fa8cbc4b7ef83010ec90f2de53a828
Modest Meowth <modest@meowth.dev>
2026-05-26 11:39:45 -0500

    more hyprland window rules

---
+++ dotfiles/hypr/conf/rules/bitwarden.lua
@@ -0,0 +1,11 @@
+hl.window_rule({
+  match = { class = "^(Bitwarden)$" },
+  no_screen_share = true,
+  tag = "+floating-window"
+})
+
+hl.window_rule({
+  match = { class = "chrome-nngceckbapebfimnlniiiahkandclblb-*" },
+  no_screen_share = true,
+  tag = "+floating-window"
+})
---
+++ dotfiles/hypr/conf/rules/steam.lua
@@ -0,0 +1,22 @@
+hl.window_rule({
+  match = { class = "steam" },
+  float = true,
+  idle_inhibit = "fullscreen",
+})
+
+hl.window_rule({
+  match = { class = "steam", title = "steam" },
+  center = true,
+  size = { 1100, 700 },
+})
+
+hl.window_rule({
+  match = { class = "steam.*" },
+  tag = "-default-opacity",
+  opacity = "1 1",
+})
+
+hl.window_rule({
+  match = { class = "steam", title = "Friends List" },
+  size = { 460, 800 },
+})
