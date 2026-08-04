39f53c29d8b27619d74ae656d9c8456cb4ecf182
Modest Meowth <modest@meowth.dev>
2026-08-04 10:19:23 -0500

    update flake

--- modules/community/theming/catppuccin/_quickshell.nix
+++ modules/community/theming/catppuccin/_quickshell.nix
@@ -7,7 +7,7 @@ let
   palette = (lib.importJSON "${sources.palette}/palette.json").${flavor}.colors;
 
   quickshellTheme = builtins.concatStringsSep "\n"
-    (lib.mapAttrsToList (n: v: "  property color ${n}: ${v.hex}") palette);
+    (lib.mapAttrsToList (n: v: ''  property color ${n}: "${v.hex}"'') palette);
 in
 {
   xdg.configFile."quickshell/Theme.qml".text = # qml
@@ -15,12 +15,40 @@ in
       import QtQuick
       
       QtObject {
-      ${quickshellTheme};
+        ${quickshellTheme};
 
         property color accent: ${accent}
-        property color shadow: lavender
 
-        property string font: "${fonts.monospace.name}"
+        property color bg: base
+        property color bgDim: mantle
+        property color bgHover: surface0
+        property color bgFocus: surface1
+        property color fg: lavender
+        property color fgMuted: surface2
+        property color fgDim: text
+        property color fgFocus: rosewater
+
+        property color highlight: base
+        property color shadow: lavender
+        property color border: surface1
+        property color selection: blue
+
+        property color launcherBg: lavender
+        property color launcherPrompt: green
+        property color launcherText: mantle
+        property color launcherSelectBlue: blue
+
+        property color clipboardBg: lavender
+        property color clipboardPrompt: green
+        property color clipboardText: mantle
+
+        property color topbarBg: lavender
+        property color topbarWidgetBg: lavender
+        property color topbarSliderBg: lavender
+        property color topbarClockBg: lavender
+        
+        property string monospaceFont: "${fonts.monospace.name}"
+        property string sansSerifFont: "${fonts.sansSerif.name}"
       }
   '';
 }
